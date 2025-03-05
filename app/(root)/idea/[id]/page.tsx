aefcd63bd3bf01bdaf880b0d9a1cc8dd961e3c11
Tristan Sim <93330584+ssim3@users.noreply.github.com>
2025-03-05 16:25:07 +0800

    Changes to Idea page frontend

--- app/(root)/idea/[id]/page.tsx
+++ app/(root)/idea/[id]/page.tsx
@@ -8,6 +8,7 @@ import { notFound } from "next/navigation";
 import React from "react";
 
 import markdownit from "markdown-it";
+import Link from "next/link";
 
 const md = markdownit();
 
@@ -26,39 +27,46 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
     <>
       <section className="m-auto flex min-h-80 max-w-5xl flex-col justify-center gap-10 p-10">
         {/* Heading Title   */}
-        <div
-          id="#heading"
-          className="flex flex-wrap items-start justify-between gap-5"
-        >
-          <div className="flex flex-col gap-5">
-            <h1 className="text-3xl text-rose-500">{post.title}</h1>
-            <div className="flex items-center justify-start gap-5">
-              <Image
-                className="rounded-full"
-                src={post?.author?.image}
-                alt="Author Profile Picture"
-                width={36}
-                height={36}
-              />
-              <div>
-                <p>{post.author?.name}</p>
-              </div>
+        <div className="flex flex-col gap-10">
+          <div
+            id="#heading"
+            className="flex flex-wrap items-end justify-between gap-5"
+          >
+            <h1 className="text-5xl font-bold text-rose-500">{post.title}</h1>
+            <Link href={`/?search=${post.category}`}>
+              <p className="text-sm font-bold italic">{post.category}</p>
+            </Link>
+          </div>
+
+          <div className="flex items-center justify-start gap-5">
+            <Image
+              className="rounded-full"
+              src={post?.author?.image}
+              alt="Author Profile Picture"
+              width={36}
+              height={36}
+            />
+            <div>
+              <p className="text-sm text-gray-500">
+                Posted by {post.author?.name}, at {post._createdAt}
+              </p>
             </div>
           </div>
-          <p>posted on {post._createdAt}</p>
         </div>
 
         {/* Banner and Interactions   */}
         <div className="flex flex-col flex-wrap items-center justify-between gap-16 md:flex-row">
           <img
-            className="obc flex-1 rounded-2xl"
+            className="w-full max-w-md rounded-2xl object-cover"
             src={post?.image}
             alt="Idea Image"
-            width="300px"
+            style={{
+              aspectRatio: "3/2",
+            }}
           />
           <div className="flex flex-1 flex-col gap-10">
-            <p>{post.description}</p>
-            <div className="flex flex-wrap items-center justify-start gap-5 sm:gap-10">
+            <p className="text-center">{post.description}</p>
+            <div className="flex flex-wrap items-center justify-center gap-5 sm:gap-10">
               <button className="flex w-1/2 max-w-40 items-center justify-center gap-2 rounded-3xl bg-rose-500 px-4 py-3 transition-transform hover:scale-105">
                 Like
                 <Heart width={20} />
@@ -70,7 +78,9 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
             </div>
           </div>
         </div>
+      </section>
 
+      <section className="m-auto flex max-w-5xl flex-col justify-center gap-10 p-10">
         <div>
           <h2 className="mb-5 text-2xl text-rose-500">Pitch</h2>
           {parsedContent ? (
@@ -81,11 +91,15 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
             <p>Pitch not found...</p>
           )}
         </div>
+      </section>
 
+      {/* TO BE IMPLEMENTED
+      <section className="m-auto flex max-w-5xl flex-col justify-center gap-10 p-10">
         <div>
           <h2 className="text-2xl text-rose-500">Comments</h2>
         </div>
       </section>
+      */}
     </>
   );
 };
