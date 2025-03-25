ff71f8fbe06904e0253290b5e6fd5f853cd424f0
ssim3 <93330584+ssim3@users.noreply.github.com>
2025-03-25 14:32:27 +0800

    Ignore Build Errors

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,19 @@ import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript : {
+  typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
   experimental: {
+    // Check if 'incrementalCacheHandler' is what you meant
     ppr: "incremental",
   },
-  devIndicators: false,
-};
-
-module.exports = {
+  devIndicators: {
+    buildActivity: false, // Correct format
+  },
   images: {
     remotePatterns: [
       {
@@ -27,7 +27,6 @@ module.exports = {
       },
     ],
   },
-
   env: {
     SANITY_API_WRITE_TOKEN: process.env.SANITY_API_WRITE_TOKEN,
     NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
@@ -37,24 +36,11 @@ module.exports = {
   },
 };
 
-module.exports = withSentryConfig(nextConfig, {
+export default withSentryConfig(nextConfig, {
   org: "tristan-sim",
   project: "javascript-nextjs",
   silent: !process.env.CI,
   widenClientFileUpload: true,
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
   automaticVercelMonitors: true,
 });
